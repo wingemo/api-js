e5678d24e1015c1903ae89d18d99f1f96babec51
Wingemo <philip.wingemo@outlook.com>
2023-04-07 01:14:08 +0200

    Update monthlyExpensesList.js

--- monthlyExpensesList.js
+++ monthlyExpensesList.js
@@ -1,21 +1,27 @@
-function monthlyExpensesList(data) {
-  const expensesByMonth = {};
+async function monthlyExpensesList(data) {
+  return new Promise((resolve, reject) => {
+    const expensesByMonth = {};
 
-  data.transactions.forEach(transaction => {
-    const date = new Date(transaction.dates.booked);
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const expense = transaction.amount.value.unscaledValue / 100;
-  
-    if (!expensesByMonth[year]) {
-      expensesByMonth[year] = {};
+    try {
+      data.transactions.forEach(transaction => {
+        const date = new Date(transaction.dates.booked);
+        const year = date.getFullYear();
+        const month = date.getMonth() + 1;
+        const expense = transaction.amount.value.unscaledValue / 100;
+      
+        if (!expensesByMonth[year]) {
+          expensesByMonth[year] = {};
+        }
+        if (!expensesByMonth[year][month]) {
+          expensesByMonth[year][month] = 0;
+        }
+      
+        expensesByMonth[year][month] += expense;
+      });
+
+      resolve(expensesByMonth);
+    } catch (error) {
+      reject(error);
     }
-    if (!expensesByMonth[year][month]) {
-      expensesByMonth[year][month] = 0;
-    }
-  
-    expensesByMonth[year][month] += expense;
   });
-
-  return monthlyExpensesList;
 }
