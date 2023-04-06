c3a0d29148b213142bc2b94fdada8fe9ade4fc8d
Wingemo <philip.wingemo@outlook.com>
2023-04-07 01:12:47 +0200

    Update dataProcessor.js

--- dataProcessor.js
+++ dataProcessor.js
@@ -1,1 +1,14 @@
-
+/**
+ * Main function that loads the transaction data from a file, parses the monthly expenses, and logs
+ * the resulting object to the console.
+ * @param {string} filepath - The path to the file containing the transaction data.
+ */
+async function run(filepath) {
+  try {
+    const transactions = await fetchTransactions(filepath);
+    const expensesByMonth = parseMonthlyExpenses(transactions);
+    console.log(expensesByMonth);
+  } catch (error) {
+    console.error(error);
+  }
+}
