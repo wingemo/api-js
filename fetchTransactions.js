43620ac29a280ba2bc8ef049e50fd5836c3a4c83
Wingemo <philip.wingemo@outlook.com>
2023-04-07 00:58:11 +0200

    Create fetchTransactions.js

---
+++ fetchTransactions.js
@@ -0,0 +1,27 @@
+/**
+ * Parses the JSON response and returns the transactions array.
+ * @param {string} response - The JSON-formatted response string.
+ * @returns {Array} An array of transaction objects.
+ */
+function parseResponse(response) {
+    const data = JSON.parse(response);
+    const transactions = data.transactions;
+    return transactions;
+}
+
+/**
+ * Fetches transactions by making two API requests with preconfigured settings
+ * (config1 and config2) and returns the parsed transactions array.
+ * @returns {Promise<Array>} A promise that resolves with an array of transaction objects.
+ */
+const fetchTransactions = async () => {
+    try {
+        const accessToken = await makeRequest(config1);
+        const listTransactions = await makeRequest(config2);
+        return parseResponse(transactions);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export default fetchTransactions;
